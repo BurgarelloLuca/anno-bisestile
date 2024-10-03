63fca3c8726d40fb00a5092565919d56b7f6bb9d
BurgarelloLuca <burgarelloluca668@gmail.com>
2024-10-03 15:13:51 +0200

    es 3.20

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int anno;
+    printf("inesrire l' anno");
+    scanf("%d",&anno);
+    if(anno%4==0 && anno%400==0) {
+        printf("l' anno e' bisestile");
+    }
+    else {
+        printf("l' anno non e' bisestile");
+    }
+    return 0;
+}
